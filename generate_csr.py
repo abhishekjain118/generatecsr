b31fbe37d045c9ed9547bb0b564ebbe1c054a01d
Abhishek Jain <abhishekjain118@gmail.com>
2018-10-04 15:23:38 +0100

    create folder is generic now

--- generate_csr.py
+++ generate_csr.py
@@ -3,18 +3,18 @@ import time
 from datetime import datetime, date, time
 import re
 
-def create_folder_cnf(base_path, folder_name): #done
-    cmd_mkdir = "mkdir " + base_path + folder_name
+def create_folder(folder_name): #done
+    cmd_mkdir = "mkdir " +folder_name
     process = subprocess.Popen(cmd_mkdir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = process.communicate()
     if re.search(r'File exists',str(error)):
         print("Folder Exists: renaming existing "+folder_name+" folder and creating a new "+folder_name)
-        cmd_mv = "mv "+base_path+folder_name+" "+base_path+folder_name+"_"+datetime.utcnow().strftime('%d%m%y_%H%M%S')
+        cmd_mv = "mv "+folder_name+" "+folder_name+"_"+datetime.utcnow().strftime('%d%m%y_%H%M%S')
         #rename existing cnf folder as cnf_<today's date and time>
         process = subprocess.Popen(cmd_mv, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = process.communicate()
         print(output, error)
-        cmd_mkdir = "mkdir " + base_path + folder_name
+        cmd_mkdir = "mkdir " + folder_name
         process = subprocess.Popen(cmd_mkdir, stdout=subprocess.PIPE, stderr=subprocess.PIPE) #now makedir cnf
         output, error = process.communicate()
     else:
@@ -45,9 +45,9 @@ def read_config_files(): #done
 
         return fqdns, kodiak_vms_in_fqdns, values_in_config_files
 
-def generate_cnf_files(fqdn,values_in_config_files,cnf_folder_path):
+def generate_cnf_files(fqdn,values_in_config_files):
 
-    out_cnf_file = open(cnf_folder_path+fqdn.split('.')[0]+".cnf", "x")
+    out_cnf_file = open("cnf\\"+fqdn.split('.')[0]+".cnf", "x")
     out_cnf_file.write("[req]\nprompt = no\ndistinguished_name = dn\nreq_extensions = ext\n")
     out_cnf_file.write("[dn]\n" +
           "CN = " + fqdn
@@ -59,25 +59,41 @@ def generate_cnf_files(fqdn,values_in_config_files,cnf_folder_path):
           + "\n\n[ext]\n" +
           "subjectAltName=DNS:" + fqdn)
 
+def generate_private_key(kodiak_vm,base_path):
+    key_path = "key_and_csr\\"
+    cmd_genrsa = base_path+"openssl genrsa -out "+key_path+kodiak_vm+".key 2048"
+    process = subprocess.Popen(cmd_genrsa.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    if re.search(r'Generating RSA private key', str(error)):
+        print("generate private key success")
+    else:
+        print("generate private key failed.")
+        print(error)
+
+
 def main():
     #date_time = datetime.utcnow().strftime('%d%m%y_%H%M%S')
     #print(date_time)
-
     base_path = "C:\\OpenSSL-Win64\\bin\\"
     folder_name = "cnf"
-    create_folder_cnf(base_path, folder_name)
 
-    fqdns, kodiak_vms_in_fqdns, values_in_config_files = read_config_files()
+    create_folder(folder_name) #create cnf folder. rename old cnf if exists.
+    fqdns, kodiak_vms_in_fqdns, values_in_config_files = read_config_files() #read config and fqdns files.
+
     print(fqdns)
     print(kodiak_vms_in_fqdns)
     print(values_in_config_files)
 
-    cnf_folder_path = base_path+folder_name+"\\"
+    #cnf_folder_path = base_path+folder_name+"\\"
     #print(cnf_folder_path)
     for fqdn in fqdns:
-        generate_cnf_files(fqdn,values_in_config_files,cnf_folder_path)
+        generate_cnf_files(fqdn,values_in_config_files) #generate cnf files and put them in cnf folder
+
+    create_folder("key_and_csr")
+    for kodiak_vm in kodiak_vms_in_fqdns:
+        generate_private_key(kodiak_vm,base_path)
 
 
 if __name__ == "__main__":
-    output_error = main()
+    main()
     #print(output_error)
